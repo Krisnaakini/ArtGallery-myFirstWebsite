a90f07203894f150d84382010ed8bc747dcbdb6e
Krisnaakini <110580278+Krisnaakini@users.noreply.github.com>
2023-02-04 11:45:07 +0530

    Add files via upload

---
+++ JavaScriptFiles/message.js
@@ -0,0 +1,11 @@
+
+function openinfo()
+{
+    document.getElementById("infobox").style.display = "block";
+}
+
+function closeinfo()
+{
+    document.getElementById("infobox").style.display = "none";
+}
+
