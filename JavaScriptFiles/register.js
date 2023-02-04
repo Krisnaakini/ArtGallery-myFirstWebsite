b6520c135139497e548efb7f652af70317a579a7
Krisnaakini <110580278+Krisnaakini@users.noreply.github.com>
2023-02-04 11:53:46 +0530

    Add files via upload

---
+++ JavaScriptFiles/register.js
@@ -0,0 +1,11 @@
+function galList(){
+    document.getElementById("galmenu").classList.toggle("show");
+}
+
+function openForm(){
+    document.getElementById("reg_form").style.display = "block";
+}
+
+function closeForm(){
+    document.getElementById("reg_form").style.display = "none";
+}
